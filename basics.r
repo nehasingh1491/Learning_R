d844e5f38cc9d83165c24e1b4a33c8c6c4193280
nehasingh1491 <nehasingh1491@gmail.com>
2023-06-13 19:43:01 +0200

    Update basics.r
    Co-authored-by: Alberto Bacchelli <sback@users.noreply.github.com>

--- basics.r
+++ basics.r
@@ -3,7 +3,7 @@ install.packages("lubridate")
 
 library("data.table")
 library("lubridate")
-#added for testing
+#added for testing and so on and so forth
 
 #loading data
 df <- fread("<path_to_file>/transactions.csv")
